2a2462516f4df3fe91242fde8b3425833ba02641
Emilijan Koteski <emilijan.koteski@teaming.inteligenta.io>
2023-02-13 15:35:51 +0100

    Added starting files

---
+++ app/index.php
@@ -0,0 +1,27 @@
+<?php include 'inc/header.php'; ?>
+
+<img src="/plain-php/app/img/Na_Dec_46.jpg" class="w-25 mb-3"
+     alt="young-businessman-was-admired-from-anypeople-with-thump-up-business-successful-concept-cartoon-character-vector-illustration">
+<a href="https://www.freepik.com/free-vector/young-businessman-was-admired-from-anypeople-with-thump-up-business-successful-concept-cartoon-character-vector-illustration_22821994.htm#query=feedback&position=3&from_view=search&track=sph">Image
+    by jcomp</a> on Freepik
+<h2>Feedback</h2>
+<p class="lead text-center">Leave feedback for Plain PHP</p>
+<form action="" class="mt-4 w-75">
+    <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+    </div>
+    <div class="mb-3">
+        <label for="body" class="form-label">Feedback</label>
+        <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback"></textarea>
+    </div>
+    <div class="mb-3">
+        <input type="submit" name="submit" value="Send" class="btn btn-warning w-100">
+    </div>
+</form>
+
+<?php include 'inc/footer.php'; ?>
